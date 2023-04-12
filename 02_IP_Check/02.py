dd41f0589c7f0cda6eb548dfa27f0fbd8a2fd887
gitteor <21cscientist@gmail.com>
2023-04-12 19:34:15 +0900

    feat

--- 02_IP_Check/02.py
+++ 02_IP_Check/02.py
@@ -2,12 +2,13 @@ import socket
 import requests
 import re
 
-in_ip_addr = socket.gethostbyname(socket.gethostname())
+# 내부 아이피
+# in_ip_addr = socket.gethostbyname(socket.gethostname())
+in_ip_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+in_ip_addr.connect(("www.google.com", 443))
+print(in_ip_addr.getsockname()[0])
 
+# 외부 아이피
 reqst = requests.get("http://ipconfig.kr")
 out_ip_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}.\d{1,3}.\d{1,3})', reqst.text)[1]
-
-# 내부 아이피
-# print(in_ip_addr)
-# 외부 아이피
 print(out_ip_addr)
